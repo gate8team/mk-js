b470aa1f5f3ed94dd631be38c2fa65b8c25d1915
antonshabanov <me.antony.rain@gmail.com>
2014-10-29 17:36:28 +0300

    [UPDATED]: updated structure.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
     var mkJS = new MKjs({
         container: '.b-toasty',
-        soundToPlay: './static/sounds/toasty.mp3'
+        soundToPlay: './static/sounds/toasty.mp3',
+        startInterval: 300,
+        endInterval: 300
     });
 
     $('.b-navigation').click(function(){
-        mkJS.playSound(null);
-        //playSound('./static/sounds/toasty.mp3');
-        $('.b-toasty').animate({
-            opacity: 1,
-            bottom: '-60px'
-        }, 300);
+        mkJS.playSound(null)
+            .toastyIn();
     });
 
     $('.b-toasty').click(function(){
-        $(this).animate({
-            bottom: '-100%'
-        }, 300);
+        mkJS.toastyOut();
     });
 });
--- src/static/js/mkjs.js
+++ src/static/js/mkjs.js
@@ -1,10 +1,12 @@
 ; 'use strict';
 var MKjs = (function($){
-    var container, soundToPlay;
+    var container, soundToPlay, startInterval, endInterval;
 
     var mkjs = function(config) {
         container = config.container || '.b-mkjs';
         soundToPlay = config.soundToPlay;
+        startInterval = config.startInterval || 300;
+        endInterval = config.endInterval || 300;
     };
 
     var playSound = function(soundPath) {
@@ -16,11 +18,32 @@ var MKjs = (function($){
         $(container)
             .find('div.b-mkjs-player')
             .html('<audio autoplay="autoplay"><source src="' + sound + '" type="audio/mpeg" /><source src="' + ogg + '" type="audio/ogg" /><embed hidden="true" autostart="true" loop="false" src="' + sound +'" /></audio>');
+
+        return this;
+    };
+
+    var toastyIn = function() {
+        $(container).animate({
+            opacity: 1,
+            bottom: '-60px'
+        }, startInterval);
+
+        return this;
+    };
+
+    var toastyOut = function() {
+        $(container).animate({
+            bottom: '-100%'
+        }, endInterval);
+
+        return this;
     };
 
     mkjs.prototype = {
         constructor: mkjs,
-        playSound: playSound
+        playSound: playSound,
+        toastyIn: toastyIn,
+        toastyOut: toastyOut
     };
 
     return mkjs;
